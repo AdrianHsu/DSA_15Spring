9132d70e9dd8565ae5a42c10b911b8b7de40b027
AdrianHsu <adrianhsu1995@gmail.com>
2015-03-28 23:46:39 +0800

    something works: if no repeated part, using unordered_map

--- HW#2/submit/b03901023.cpp
+++ HW#2/submit/b03901023.cpp
@@ -16,71 +16,67 @@
 using namespace std;
 typedef unordered_map<string, vector<HashNode>> _HashMap;
 
-bool string2Int(const string&, int*);
-bool insert(_HashMap&, string, HashNode*);
+unsigned int string2Int(const string&, unsigned int&);
+bool insert(_HashMap&, HashNode*);
 int main()
 {
 	_HashMap hashMap;
-	string path =  "test_1000.txt";
+	string path =  "test_235466.txt";
 
 	//import
 	ifstream ifs;
 	ifs.open( path.c_str(), ifstream::in);
-	if(!ifs.is_open())
+	if (!ifs.is_open())
 		cout << "ERROR: open file failed" << endl;
-	
+
+	int count = 0;
 	string line;
-	while(getline(ifs, line)) //getline() reads from ifs a line, and store it into "line"
+	while (getline(ifs, line)) //getline() reads from ifs a line, and store it into "line"
 	{
 		istringstream ss(line);
 		HashNode* temp = new HashNode;
+
 		int i = 0;
-		while( i < 12 )
+		while ( i < 12 )
 		{
 			ss >> temp->getElement(i);
-			//insert(hashMap, key, temp);
-			//if(i == 11)delete temp;
-			cout << i << " " << temp->getElement(i) << endl; 
-
+			if (i == 11)
+			{
+				string key = temp->getElement(11);
+				if ( hashMap.count(key) == 0)
+				{
+					vector<HashNode>* _vec = new vector<HashNode>;
+					_vec->push_back(*temp);
+					hashMap.insert( pair<string, vector<HashNode>>( key , *_vec ) );
+					count++;
+				} else
+				{
+					/*if(hashMap[temp->getElement(11)])
+					hashMap[temp->getElement(11)].push_back(*temp);*/
+				}
+			}
+			//cout << i << " " << temp->getElement(i) << endl;
 			i++;
 		}
 	}
+	for (auto& x: hashMap) {
+    cout << x.first << ": " << x.second[0].getElement(2)<< endl;
+  	}
 	ifs.close();
 	return 0;
 }
 
-/*bool insert(_HashMap& hashMap, int* n, HashNode* node)
-{
-	_HashMap::hasher fn = hashMap.hash_function();
-	int k = *n;
+bool insert(_HashMap& hashMap, HashNode* node)
+{	return true;}
 
-	cout << *n << " - " << fn( k ) << endl;
-	//longint flag = hashMap[ fn( (longint)*n ) ];
-	if( flag != 0)
-	{
-		vector<HashNode> test;
-		test.push_back(*node);
-		hashMap.insert( pair<longint, vector<HashNode>>( (longint)*n, test ) );
-		return true;
-	}else
-	{
-		return false;
+unsigned int string2Int(const string& str, unsigned int& num)
+{
+	num = 0;
+	for (unsigned int i = 0; i < str.size(); i++) {
+		if (isdigit(str[i])) {
+			num *= 10;
+			num += int(str[i] - '0');
+		}
 	}
-	return false;
-
-}*/
-
-bool string2Int(const string& str, int* num)
-{
-   *num = 0;
-   bool flag = true;
-   for (unsigned int i = 0; i < str.size(); i++) {
-      if (isdigit(str[i])) {
-         *num *= 10;
-         *num += int(str[i] - '0');
-         flag = true;
-      }
-      else return false;
-   }
-   return flag;
+	return num;
 }
