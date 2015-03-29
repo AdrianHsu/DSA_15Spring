ecc0f244430e41aff693b9bbe535d64e832464a8
AdrianHsu <adrianhsu1995@gmail.com>
2015-03-29 22:52:55 +0800

    import() function, to make main() cleaner

--- HW#2/submit/b03901023.cpp
+++ HW#2/submit/b03901023.cpp
@@ -14,6 +14,22 @@
 //#define _HASH_MAX  134217728 //2^27
 #define _HASH_MAX    25000000 //suppose unordered_mapered_maps are at most 20000000(apprpximately)
 
+using namespace std;
+typedef unordered_map< string, vector<HashNode> > _HashMap;
+typedef unsigned int longint;
+
+bool import(string&);
+longint string2Int(const string&, longint&);
+longint string2Int(const string&);
+string int2String(longint &);
+double string2Double(const string&);
+
+void get(string, string, string, string, string);
+void clicked(string);
+void impressed(string, string);
+void profit(double, double);
+
+static _HashMap hashMap;
 /*
 terminate called after throwing an instance of 'std::bad_alloc'
   what():  std::bad_alloc
@@ -32,21 +48,7 @@ user    8m6.852s
 sys     0m49.444s
 
 */
-using namespace std;
-typedef unordered_map< string, vector<HashNode> > _HashMap;
-typedef unsigned int longint;
 
-longint string2Int(const string&, longint&);
-longint string2Int(const string&);
-double string2Double(const string&);
-
-string int2String(longint &);
-void get(string, string, string, string, string);
-void clicked(string);
-void impressed(string, string);
-void profit(double, double);
-
-static _HashMap hashMap;
 int main()
 {
 
@@ -54,12 +56,40 @@ int main()
 	//string path = "kddcup2012track2_7924321.txt";
 	string path =  "test_235466.txt";
 
+	if(import(path))
+		printf("DONE!\n");
+
+	longint _line = 0;
+	for (auto& x : hashMap) {
+		_line += x.second.size();
+	}
+	
+	get("6231944", "20157628", "0", "3", "3");//(0, 1)
+	get("2647468", "20711336", "22138939", "1", "1");//line 100000, (0, 1)
+	get("516038", "6434912", "1205", "2", "2");//line 100000, (0, 1)
+	clicked("11416328"); //line 226448
+	clicked("67385"); //line 227151
+	impressed("490234", "86056");
+	impressed("67385", "388585"); //line 67385, 227370
+	impressed("55984", "872615");
+	double a = 0;
+	profit(a, 1.5);
+
+	printf( "TOTAL_LINE: %u\n", _line);
+	printf( "MAP_SIZE: %u\n", hashMap.size());
+	return 0;
+}
+
+bool import(string& path)
+{
 	//import
 	ifstream ifs;
 	ifs.open( path.c_str(), ifstream::in);
 	if (!ifs.is_open())
+	{
 		printf( "ERROR: open file failed\n");
-
+		return false;
+	}
 	string line;
 	while (getline(ifs, line)) //getline() reads from ifs a line, and store it into "line"
 	{
@@ -107,28 +137,8 @@ int main()
 		}
 	}
 	ifs.close();
-	printf("DONE!\n");
-
-	longint _line = 0;
-	for (auto& x : hashMap) {
-		_line += x.second.size();
-	}
-	get("6231944", "20157628", "0", "3", "3");//(0, 1)
-	get("2647468", "20711336", "22138939", "1", "1");//line 100000, (0, 1)
-	get("516038", "6434912", "1205", "2", "2");//line 100000, (0, 1)
-	clicked("11416328"); //line 226448
-	clicked("67385"); //line 227151
-	impressed("490234", "86056");
-	impressed("67385", "388585"); //line 67385, 227370
-	impressed("55984", "872615");
-	double a = 0;
-	profit(a, 1.5);
-
-	cout << "TOTAL_LINE: " <<  _line << endl;
-	printf( "MAP_SIZE: %u\n", hashMap.size());
-	return 0;
+	return true;
 }
-
 longint string2Int(const string& str, longint& num)
 {
 	num = 0;
@@ -139,17 +149,6 @@ longint string2Int(const string& str, longint& num)
 		}
 	}
 	return num;
-}
-double string2Double(const string& str)
-{
-	longint num = 0;
-	for (longint i = 0; i < str.size(); i++) {
-		if (isdigit(str[i])) {
-			num *= 10;
-			num += int(str[i] - '0');
-		}
-	}
-	return (double)num;
 }
 longint string2Int(const string& str)
 {
@@ -170,6 +169,17 @@ string int2String(longint &i) {
 
 	return ss.str();
 }
+double string2Double(const string& str)
+{
+	longint num = 0;
+	for (longint i = 0; i < str.size(); i++) {
+		if (isdigit(str[i])) {
+			num *= 10;
+			num += int(str[i] - '0');
+		}
+	}
+	return (double)num;
+}
 void get(string u, string a, string q, string p, string d)
 {
 	if (hashMap.count(u) == 0) return;
@@ -181,11 +191,9 @@ void get(string u, string a, string q, string p, string d)
 		        && hashMap[u][i].getElement(6) == p && hashMap[u][i].getElement(5) == d)
 		{
 			printf( "(%s, %s)\n", hashMap[u][i].getElement(0).c_str(), hashMap[u][i].getElement(1).c_str());
-			//break; // maybe more than one set
 		}
 		i++;
 	}
-
 	return;
 }
 void clicked(string u)
@@ -236,7 +244,6 @@ void impressed(string u1, string u2)
 			}
 		}
 	}
-
 	longint i, j, k;
 	for (k = vec_Ptr.size() / 2; k > 0; k /= 2) {
 		for (i = k; i < vec_Ptr.size(); i++) {
