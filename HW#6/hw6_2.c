dcf6c291e5deca96a888b24160be1e9a80dd5ed1
AdrianHsu <adrianhsu1995@gmail.com>
2015-06-01 22:22:17 +0800

    hw6_2 done

--- HW#6/hw6_2.c
+++ HW#6/hw6_2.c
@@ -2,20 +2,55 @@
 #include <stdlib.h> 
 #include <string.h>
 #include "avl.h"
+#include "bst.h"
+#include "rb.h"
+
 
 #define MAX_STRLEN 150 //128 as MAX
 
-void  preorder_integer_avl(const struct avl_node *node)
+void preorder_integer_avl(const struct avl_node *node)
 {
 	if(node == NULL)
 		return;
+
 	printf("%s ", (char *)node->avl_data);
 	if(node->avl_link[0] != NULL || node->avl_link[1] != NULL)
 	{
 		putchar('(');
 		preorder_integer_avl(node->avl_link[0]);
 		putchar(',');
+		putchar(' ');
 		preorder_integer_avl(node->avl_link[1]);
+		putchar(')');
+	}
+}
+void preorder_integer_bst(const struct bst_node *node)
+{
+	if(node == NULL)
+		return;
+	printf("%s ", (char *)node->bst_data);
+	if(node->bst_link[0] != NULL || node->bst_link[1] != NULL)
+	{
+		putchar('(');
+		preorder_integer_bst(node->bst_link[0]);
+		putchar(',');
+		putchar(' ');
+		preorder_integer_bst(node->bst_link[1]);
+		putchar(')');
+	}
+}
+void preorder_integer_rb(const struct rb_node *node)
+{
+	if(node == NULL)
+		return;
+	printf("%s ", (char *)node->rb_data);
+	if(node->rb_link[0] != NULL || node->rb_link[1] != NULL)
+	{
+		putchar('(');
+		preorder_integer_rb(node->rb_link[0]);
+		putchar(',');
+		putchar(' ');
+		preorder_integer_rb(node->rb_link[1]);
 		putchar(')');
 	}
 }
@@ -26,6 +61,7 @@ int dic_compare(const void *pa, const void *pb, void * param)
 
 	const int str1_len = strlen(str1);
 	const int str2_len = strlen(str2);
+
 	int i = 0;
 	while(1)
 	{
@@ -36,40 +72,43 @@ int dic_compare(const void *pa, const void *pb, void * param)
 			return -1;
 		else if(i == str2_len)
 			return +1;
-
+		
 		if(str1[i] < str2[i])
-			return +1;
-		else if(str1[i] > str2[i])
 			return -1;
-		else
-			i++;
+		else if(str1[i] > str2[i])
+			return +1;
+
+		i++;
 	}
 	printf("ERROR\n");
 	return 0;
 
 }
 
-/*int int_compare(const void *pa, const void *pb, void * param)
-{
-	int a = *(const int*)pa;
-	int b = *(const int*)pb;
-	if(a < b) return -1;
-	else if (a > b)return +1;
-	else return 0;
-}*/
-
 int main(){
 	
-	struct avl_table *tree ;
-	tree = avl_create(dic_compare , NULL, NULL);
+	struct avl_table *avl_tree ;
+	struct bst_table *bst_tree ;
+	struct rb_table *rb_tree ;
+	avl_tree = avl_create(dic_compare , NULL, NULL);
+	bst_tree = bst_create(dic_compare , NULL, NULL);
+	rb_tree = rb_create(dic_compare , NULL, NULL);
 	
-	char S1 [MAX_STRLEN];
-	while(scanf("%s", S1) == 1)
+	for(int i = 0; i < 32; i++)
 	{
-		char* element = S1;
-		void** p = avl_probe(tree, element);
+		char *element = (char*)malloc(sizeof(char)* MAX_STRLEN);
+		if(scanf("%s", element) == 1)
+		{
+			void** p = avl_probe(avl_tree, element);
+			void** q = bst_probe(bst_tree, element);
+			void** r = rb_probe(rb_tree, element);
+		}
 	}
-	preorder_integer_avl(tree->avl_root);
+	preorder_integer_avl(avl_tree->avl_root);
+	printf("\n\n");
+	preorder_integer_bst(bst_tree->bst_root);
+	printf("\n\n");
+	preorder_integer_rb(rb_tree->rb_root);
 	puts("");
 	return 0 ;
 }
