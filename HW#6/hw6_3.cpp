37702f876e9b9f8631228af16661f8944bb03eb4
AdrianHsu <adrianhsu1995@gmail.com>
2015-06-02 21:37:13 +0800

    almost done

--- HW#6/hw6_3.cpp
+++ HW#6/hw6_3.cpp
@@ -5,6 +5,7 @@ extern "C" {
 }
 
 using namespace std;
+typedef unsigned long long long_int;
 
 class Game{
 
@@ -20,17 +21,22 @@ public:
 
 Game disjoint_Set[110000]; //disjoint_Set[i] means i-th game
 int _NUM_OF_OWNER = 0;
+int _TOTAL_GAME = 0;
 
 int find_owner(int);
 void owner_identified();
 void first_incident(int , int);
+
+void inorder_integer_avl(const struct avl_node *,const long_int , long_int *, int *);
+int int_compare(const void *, const void *, void *);
+void second_incident(int , long_int);
 
 int main()
 {
 
 	int n = 0, m = 0;
 	scanf("%d%d",&n, &m);
-	_NUM_OF_OWNER = n;
+	_TOTAL_GAME = _NUM_OF_OWNER = n;
 
 	disjoint_Set[0] = Game(-1, -1);
 
@@ -53,10 +59,10 @@ int main()
 		}
 		else if(incident == "2")
 		{
-			unsigned int i = 0;
-			unsigned long long s = 0;
-			scanf("%u%llu",&i, &s);
-			//second_incident(i, s);
+			int i = 0;
+			long_int s = 0;
+			scanf("%d%llu",&i, &s);
+			second_incident(i, s);
 		}
 		else
 		{
@@ -98,7 +104,58 @@ void first_incident(int i, int j)
 	disjoint_Set[ j ].owner = i;
 	_NUM_OF_OWNER--;
 	owner_identified();
-	//for(int i = 1; i <= 4; i++)
-		//cout << disjoint_Set[ i ].owner << endl;
 }
 
+void inorder_integer_avl(const struct avl_node* node,const long_int s, long_int* total , int* k)
+{
+	if(node == NULL)
+		return;
+	
+	if(node->avl_link[0] != NULL)
+	inorder_integer_avl(node->avl_link[0], s, total, k);
+	if(*total == s)
+		return;
+	else if(*total > s)
+	{
+		(*k)--;
+		return;
+	}
+	
+	*total += node->avl_data;
+	(*k)++;
+	if(*total == s)
+		return;
+	else if(*total > s)
+	{
+		(*k)--;
+		return;
+	}
+	if(node->avl_link[1] != NULL)
+	inorder_integer_avl(node->avl_link[1], s, total, k);
+}
+int int_compare(const void *pa, const void *pb, void * param)
+{
+	long_int a = *(const long_int *)pa;
+	long_int b = *(const long_int *)pb;
+	if(a < b) return -1;
+	else if (a > b)return +1;
+	else return 0;
+}
+void second_incident(int i, long_int s)
+{
+	i = find_owner(i);
+	struct avl_table *avl_tree ;
+	avl_tree = avl_create(int_compare , NULL, NULL);
+
+	for(int j = 0; j < _TOTAL_GAME; j++)
+		if(disjoint_Set[ j ].owner == i)
+		{
+			avl_probe(avl_tree, disjoint_Set[ j ].price);
+		}
+	
+	long_int* total = new long_int(0);
+	int* k = new int(0);
+	inorder_integer_avl(avl_tree->avl_root, s, total, k);
+	cout << i << " " << *k << endl;
+
+}
