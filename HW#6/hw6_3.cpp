12d12b44fb3c5c8eb3f1c047d227ce84f70ca8dd
AdrianHsu <adrianhsu1995@gmail.com>
2015-06-02 11:44:09 +0800

    second commit

--- HW#6/hw6_3.cpp
+++ HW#6/hw6_3.cpp
@@ -1,38 +1,58 @@
 #include <iostream>
 #include <string>
-#include "avl_ntudsa.h"
+extern "C" {
+	#include "avl_ntudsa.h"
+}
 
 using namespace std;
 
 typedef unsigned long long int longint;
 
+int id_compare(const void *pa, const void *pb, void * param)
+{
+	int a = *(const int*)pa;
+	int b = *(const int*)pb;
+	if(a < b) return -1;
+	else if (a > b)return +1;
+	else return 0;
+}
+
 int main()
 {
-	longint n = 0, m = 0;
-	scanf("%llu%llu",&n, &m);
 
-	longint* price = new longint [ n ];
+	unsigned int n = 0, m = 0;
+	scanf("%u%u",&n, &m);
+
+	struct avl_table **avl_tree = new avl_table* [ n ];
+	
 	for(int i = 0; i < n; i++)
-		scanf("%llu", &price[i]);
+	{
+		avl_tree[i] = avl_create(id_compare , NULL, NULL);
+		int price = 0;
+		scanf("%u", &price);
+		int* p = avl_probe(avl_tree[i], price);
+	}
 
 	string incident;
 	while(cin >> incident)
 	{
 		if(incident == "1")
 		{
-			longint i = 0, j = 0;
-			scanf("%llu%llu",&i, &j);
+			unsigned int i = 0, j = 0;
+			scanf("%u%u",&i, &j);
 			if(i == j)
 			{
 				printf("ERROR\n");
 				break;				
 			}
 
+
 		}
 		else if(incident == "2")
 		{
-			longint i = 0, s = 0;
-			scanf("%llu%llu",&i, &s);
+			unsigned int i = 0;
+			longint s = 0;
+			scanf("%u%llu",&i, &s);
 			
 		}
 		else
